04d26c41ec4a32b27dc1d407b350c08594855f10
Porszicoo <mlnr.zsomb@gmail.com>
2025-03-11 14:07:12 +0100

    kosar üriítése vasarlas után

--- frontend/src/modules/Proceed/Proceed.jsx
+++ frontend/src/modules/Proceed/Proceed.jsx
@@ -1,17 +1,28 @@
-import { useNavigate } from "react-router";
-import { useLocation } from "react-router";
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
 
 export const Proceed = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const { orderNumber, orderDate, paymentMethod, name, shippingAddress } = location.state || {};
 
+   
+    useEffect(() => {
+        localStorage.removeItem("cart"); //Kosár tartalmának ürítése
+    }, []);
+
     return (
         <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
             <div className="mx-auto max-w-2xl px-4 2xl:px-0">
-                <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl mb-2">Köszönjük a vásárlásod!</h2>
+                <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl mb-2">
+                    Köszönjük a vásárlásod!
+                </h2>
                 <p className="text-gray-500 dark:text-gray-400 mb-6 md:mb-8">
-                    A megrendelésed <a href="#" className="font-medium text-gray-900 dark:text-white hover:underline">{orderNumber}</a> 24 órán belül fel lesz dolgozva.
+                    A megrendelésed{" "}
+                    <a href="#" className="font-medium text-gray-900 dark:text-white hover:underline">
+                        {orderNumber}
+                    </a>{" "}
+                    24 órán belül fel lesz dolgozva.
                 </p>
                 <div className="space-y-4 sm:space-y-2 rounded-lg border border-gray-100 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800 mb-6 md:mb-8">
                     <div className="sm:flex items-center justify-between gap-4">
@@ -31,8 +42,13 @@ export const Proceed = () => {
                         <span className="font-medium text-gray-900 dark:text-white sm:text-end">{shippingAddress}</span>
                     </div>
                 </div>
-                <div className="flex items-center space-x-4" onClick={() => navigate('/')}>
-                    <a href="#" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Vissza a főoldalra</a>
+                <div className="flex items-center space-x-4">
+                    <button
+                        onClick={() => navigate("/")}
+                        className="py-2.5 px-5 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+                    >
+                        Vissza a főoldalra
+                    </button>
                 </div>
             </div>
         </section>
